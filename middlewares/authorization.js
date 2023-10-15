d2411329b638d67761117f0ee2c30fd3cadbb0c7
Peter Joseph <josephpeter489@gmail.com>
2023-10-15 08:31:14 +0100

    authUser

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -34,3 +34,43 @@ const authAdmin =  async (req, res, next) => {
         })
     }
 };
+
+
+const authUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const confirmUsers = await Users.findById(userId);
+        if (!confirmUsers) {
+            return res.status(400).json({
+                message: 'This user does not exist!'
+            })
+        } else {
+            const authToken = confirmUsers.token;
+            if (!authToken) {
+                return res.status(400).json({
+                    message: 'Something is wrong!'
+                })
+            }
+            jwt.verify(authToken, process.env.AUT_SEC_ID, (error, payload) => {
+                if (error) {
+                    return res.status(400).json({
+                        message: error.message
+                    })
+                } else {
+                    req.user = payload;
+                    next()
+                }
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+module.exports = {
+    authAdmin,
+    authUser
+}
