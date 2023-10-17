0e751158894ef94d9fb484993e81a4123dd9cd31
Peter Joseph <josephpeter489@gmail.com>
2023-10-17 16:50:25 +0100

    done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('./configDB/configDB.js');
 require('dotenv').config();
 const express = require('express');
-// const http = require("http")
+const http = require("http")
 const PORT = process.env.PORT || 1122
 const expressSession = require('express-session');
 const fileUpload = require('express-fileupload');
@@ -20,11 +20,10 @@ app.use(fileUpload({
     useTempFiles: true
 }));
 
-// const Server = http.createServer(app)
-// Server.listen(PORT, () => {
-//     console.log(`Listening to port: ${PORT}`);
-// });
-app.listen(PORT, ()=>{
-    console.log('This Server is running on Port: '+PORT)
+const Server = http.createServer(app)
+Server.listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}`);
 });
-
+// app.listen(PORT, ()=>{
+//     console.log('This Server is running on Port: '+PORT)
+// });
